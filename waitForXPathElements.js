0a1fa4599eb773fcd4fbd66e32528dc8aac23da3
Johannes Hoenger <johannes.hoenger@gmail.com>
2022-01-23 16:20:52 +0100

    applied formatting

--- waitForXPathElements.js
+++ waitForXPathElements.js
@@ -16,22 +16,22 @@
 'use strict';
 
 function toArray(elements) {
-    var array=[]; 
-    var element;
-    while (element=elements.iterateNext()) {
-      array.push(element);
-    }
-    return array;
+  var array = [];
+  var element;
+  while (element = elements.iterateNext()) {
+    array.push(element);
+  }
+  return array;
 }
 
-function getXPathElements(xpath){
+function getXPathElements(xpath) {
   var result = document.evaluate(
-      xpath, 
-      document, 
-      null, 
-      XPathResult.ORDERED_NODE_ITERATOR_TYPE, 
-      null);
-  
+    xpath,
+    document,
+    null,
+    XPathResult.ORDERED_NODE_ITERATOR_TYPE,
+    null);
+
   return toArray(result)
 }
 
@@ -51,30 +51,30 @@ function sleep(ms) {
 
 // returns an array of found elements
 // if the timeout elapsed without any finding NaN is returned
-async function waitForXPathElement(xpath, timeoutInMilliseconds = NaN){  
-  if(timeoutInMilliseconds){
+async function waitForXPathElement(xpath, timeoutInMilliseconds = NaN) {
+  if (timeoutInMilliseconds) {
     var now = Date.now()
     var end = now + timeoutInMilliseconds
   }
-  
+
   return _waitForXPathElement(xpath, end)
 }
 
 // the function checks the DOM using a sampling rate which scales based on
 // the fibonacci sequence
 // first it waits 0 milliseconds, then 1*100ms, then 1*100ms, then 2*100ms, 3*100ms, 5*100ms, ...
-async function _waitForXPathElement(xpath, endTime){  
+async function _waitForXPathElement(xpath, endTime) {
   var baseSampleRateInMilliseconds = 100
-  
+
   var elements = getXPathElements(xpath)
-  if(elements.length){
+  if (elements.length) {
     return elements
   } else {
     await sleep(baseSampleRateInMilliseconds * scalingFactor.next())
-    if(endTime && endTime <= Date.now()){
+    if (endTime && endTime <= Date.now()) {
       return NaN
     } else {
-      return _waitForXPathElement(xpath)      
+      return _waitForXPathElement(xpath)
     }
   }
 }
