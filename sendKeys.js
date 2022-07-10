700d2a701f014b2d214c32a68966ba9fbb5cb1da
Johannes Hoenger <johannes.hoenger@gmail.com>
2022-07-10 14:17:15 +0200

    added method to send keys to element

---
+++ sendKeys.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// offer a way to simulate key inputs with plain javascript
+function sendKeys(webelement, inputString) {
+  // loop over each character
+  for (var i = 0; i < inputString.length; i++) {
+    // generate a keyboard event
+    keyboardEvent = createKeyboardEvent(inputString.charCodeAt(i))
+    // send this event to the webelement
+    webelement.dispatchEvent(keyboardEvent)
+  }
+}
+
+function createKeyboardEvent(charCode) {
+  return new KeyboardEvent('keydown', {
+      bubbles: true, cancelable: true, keyCode: charCode
+  });
+}
