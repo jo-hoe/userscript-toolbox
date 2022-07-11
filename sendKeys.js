f484681840599e6278b013768a6888409624524d
jo-hoe <71124804+jo-hoe@users.noreply.github.com>
2022-07-11 11:42:31 +0200

    Update sendKeys.js

--- sendKeys.js
+++ sendKeys.js
@@ -1,15 +1,11 @@
 'use strict';
 
-// offers a way to simulate key inputs with plain javascript
-// note:  The created events will come with "isTrusted" to false.
-//        Manually firing a key event does NOT cause that letters to appear in a text input. 
-//        For security reasons this browser feature prevents scripts
-//        from simulating user actions that interact with the browser itself.  
+// offers a way to simulate key inputs with plain javascript  
 function sendKeys(webelement, inputString) {
   // set value first
   webelement.value = inputString
   
-  // loop over each character to fire untrusted events
+  // loop over each character to simulate keypress events
   for (var i = 0; i < inputString.length; i++) {
     // generate a keyboard event
     var keyboardEvent = createKeyboardEvent(inputString.charCodeAt(i))
@@ -20,6 +16,10 @@ function sendKeys(webelement, inputString) {
 
 // you can see the different characters here:
 // https://www.w3.org/2002/09/tests/keys.html
+// note:  The created events will come with "isTrusted" to false.
+//        Manually firing a key event does NOT cause that letters to appear in a text input. 
+//        For security reasons this browser feature prevents scripts
+//        from simulating user actions that interact with the browser itself.
 function createKeyboardEvent(charCode) {
   return new KeyboardEvent('keydown', {
       bubbles: true, cancelable: true, keyCode: charCode
